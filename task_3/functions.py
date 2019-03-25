8445dc83d1f6c0ddc96be6ae292152971a14032d
Pavel Koshkin <koshkin.pasha@gmail.com>
2019-03-25 06:19:17 +0300

    probably porks

--- task_3/functions.py
+++ task_3/functions.py
@@ -20,7 +20,6 @@ from keras.callbacks import LambdaCallback, LearningRateScheduler, Callback
 BATCH_SIZE = 64
 TEST_DATA_SIZE = 10000
 TRAIN_DATA_SIZE = 490000
-HIDDEN_SIZE = 256
 MAX_QUERY_LEN_HARD = 150
 
 def get_max_lens(filename):
@@ -69,7 +68,82 @@ def make_dense_input_from_lines(lines, query_dict_size, document_dict_size):
     )
 
     return {'query_input': query_batch, 'document_input': docment_batch}
-    
+
+
+def make_hash_input_from_lines(lines, dict_size):
+    query_indices_words = [], []
+    document_indices_words = [], []
+
+    query_indices_words_bigrams = [], []
+    document_indices_words_bigrams = [], []
+
+    query_indices_symbol_trigrams = [], []
+    document_indices_symbol_trigrams = [], []
+    for i, line in enumerate(lines):
+        query_words, query_words_bigrams, query_symbol_trigrams, document_words, document_words_bigrams, document_symbol_trigrams = line.split("\t")
+        query_words = map(int, query_words.split())
+        document_words = map(int, document_words.split())
+        query_words_bigrams = map(int, query_words_bigrams.split())
+        document_words_bigrams = map(int, document_words_bigrams.split())
+        query_symbol_trigrams = map(int, query_symbol_trigrams.split())
+        document_symbol_trigrams = map(int, document_symbol_trigrams.split())
+
+        for word in query_words:
+            query_indices_words[0].append(i)
+            query_indices_words[1].append(word)
+        for word in document_words:
+            document_indices_words[0].append(i)
+            document_indices_words[1].append(word)
+
+        for word in query_words_bigrams:
+            query_indices_words_bigrams[0].append(i)
+            query_indices_words_bigrams[1].append(word)
+        for word in document_words_bigrams:
+            document_indices_words_bigrams[0].append(i)
+            document_indices_words_bigrams[1].append(word)
+
+        for word in query_symbol_trigrams:
+            query_indices_symbol_trigrams[0].append(i)
+            query_indices_symbol_trigrams[1].append(word)
+        for word in document_symbol_trigrams:
+            document_indices_symbol_trigrams[0].append(i)
+            document_indices_symbol_trigrams[1].append(word)
+
+    query_words_batch = csr_matrix(
+        (np.ones(len(query_indices_words[0])), query_indices_words),
+        shape=(len(lines), dict_size)
+    )
+    document_words_batch = csr_matrix(
+        (np.ones(len(document_indices_words[0])), document_indices_words),
+        shape=(len(lines), dict_size)
+    )
+
+    query_words_bigrams_batch = csr_matrix(
+        (np.ones(len(query_indices_words_bigrams[0])), query_indices_words_bigrams),
+        shape=(len(lines), dict_size)
+    )
+    document_words_bigrams_batch = csr_matrix(
+        (np.ones(len(document_indices_words_bigrams[0])), document_indices_words_bigrams),
+        shape=(len(lines), dict_size)
+    )
+
+    query_symbol_trigrams_batch = csr_matrix(
+        (np.ones(len(query_indices_symbol_trigrams[0])), query_indices_symbol_trigrams),
+        shape=(len(lines), dict_size)
+    )
+    document_symbol_trigrams_batch = csr_matrix(
+        (np.ones(len(document_indices_symbol_trigrams[0])), document_indices_symbol_trigrams),
+        shape=(len(lines), dict_size)
+    )
+
+    return {
+        'query_words': query_words_batch,
+        'document_words': document_words_batch,
+        'query_words_bigrams': query_words_bigrams_batch,
+        'document_words_bigrams': document_words_bigrams_batch,
+        'query_symbol_trigrams': query_symbol_trigrams_batch,
+        'document_symbol_trigrams': document_symbol_trigrams_batch
+    }
 
 def make_lstm_dense_input_from_lines(lines, max_query_len, document_dict_size):
     document_indices_axis_0, document_indices_axis_1, document_values = [], [], []
@@ -130,6 +204,7 @@ def my_cosine_proximity(y_true, y_pred):
 
 def logloss(y_true, y_pred):
     labels = (y_true + 1) / 2
+    probs = (y_pred + 1) / 2
     return -K.mean(labels * K.log(y_pred) + (1 - labels) * K.log(1 - y_pred))
 
 def get_positive_mask(y_true):
@@ -147,25 +222,21 @@ def ranking_loss(y_true, y_pred):
 
 def mean_positive_score(y_true, y_pred):
     positives_num = K.sum(get_positive_mask(y_true))
-    negatives_num = K.sum(get_negative_mask(y_true))
-    return K.mean(y_pred * get_positive_mask(y_true)) * positives_num / (positives_num + negatives_num)
+    return K.sum(y_pred * get_positive_mask(y_true)) / positives_num
 
 def mean_positive_var(y_true, y_pred):
     mean_positive = mean_positive_score(y_true, y_pred)
     positives_num = K.sum(get_positive_mask(y_true))
-    negatives_num = K.sum(get_negative_mask(y_true))
-    return K.mean((y_pred * get_positive_mask(y_true) - mean_positive) ** 2) * positives_num / (positives_num + negatives_num)
+    return K.sum((y_pred * get_positive_mask(y_true) - mean_positive) ** 2) / positives_num
 
 def mean_negative_score(y_true, y_pred):
-    positives_num = K.sum(get_positive_mask(y_true))
     negatives_num = K.sum(get_negative_mask(y_true))
-    return K.mean(y_pred * get_negative_mask(y_true)) * negatives_num / (positives_num + negatives_num)
+    return K.sum(y_pred * get_negative_mask(y_true)) / negatives_num
 
 def mean_negative_var(y_true, y_pred):
     mean_negative = mean_negative_score(y_true, y_pred)
-    positives_num = K.sum(get_positive_mask(y_true))
     negatives_num = K.sum(get_negative_mask(y_true))
-    return K.mean((y_pred * get_negative_mask(y_true) - mean_negative) ** 2) * negatives_num / (positives_num + negatives_num)
+    return K.sum((y_pred * get_negative_mask(y_true) - mean_negative) ** 2) / negatives_num
 
 def normalize(embedding):
     return K.l2_normalize(embedding, axis=-1)
@@ -195,17 +266,19 @@ def make_lstm_branch(tensor, lstm_num, dense_num, hidden_size, dict_size, activa
 def make_dense_branch(tensor, dense_num, hidden_size, dict_size, activation):
     for i in range(dense_num):
         tensor = Dense(hidden_size, activation=activation)(tensor)  # shape: (BATCH_SIZE, hidden_size)
-    return Lambda(normalize)(tensor)  # shape: (BATCH_SIZE, hidden_size)
+    return tensor  # shape: (BATCH_SIZE, hidden_size)
 
-def make_compiled_model(input_1, input_2, embedding_1, embedding_2):
-    score = Lambda(proba_score)([embedding_1, embedding_2])
+def make_compiled_model(inputs, embedding_1, embedding_2):
+    embedding_1 = Lambda(normalize)(embedding_1)  # shape: (BATCH_SIZE, hidden_size)
+    embedding_2 = Lambda(normalize)(embedding_2)  # shape: (BATCH_SIZE, hidden_size)
+    score = Lambda(dot_product)([embedding_1, embedding_2])
     prediction = Lambda(reshape_to_prediction, name="output")(score)
 
-    model = Model(inputs=[input_1, input_2], outputs=prediction)
+    model = Model(inputs=inputs, outputs=prediction)
     model.compile(
         Adam(),
         loss=logloss,
-        metrics=[mean_positive_score, mean_negative_score, mean_positive_var, mean_negative_var, 'acc']
+        metrics=[mean_positive_score, mean_negative_score, mean_positive_var, mean_negative_var, 'acc', 'mse']
     )
     return model
 
@@ -235,8 +308,49 @@ def make_dense_model(document_dict_size,
         activation
     )
 
-    return make_compiled_model(query_input, document_input, query_embedding, document_embedding)
+    return make_compiled_model([query_input, document_input], query_embedding, document_embedding)
     
+
+def make_hash_model(dict_size,
+                    dense_num,
+                    activation,
+                    hidden_size):
+    # shape: (BATCH_SIZE, dict_size)
+    query_words_input = Input(shape=(dict_size,), sparse=True, name="query_words")
+    query_words_embedding = make_dense_branch(query_words_input, dense_num, hidden_size, dict_size, activation)
+    # shape: (BATCH_SIZE, hidden_size)
+    document_words_input = Input(shape=(dict_size,), sparse=True, name="document_words")
+    document_words_embedding = make_dense_branch(document_words_input, dense_num, hidden_size, dict_size, activation)
+
+    # shape: (BATCH_SIZE, dict_size)
+    query_words_bigrams_input = Input(shape=(dict_size,), sparse=True, name="query_words_bigrams")
+    query_words_bigrams_embedding = make_dense_branch(query_words_bigrams_input, dense_num, hidden_size, dict_size, activation)
+    # shape: (BATCH_SIZE, hidden_size)
+    document_words_bigrams_input = Input(shape=(dict_size,), sparse=True, name="document_words_bigrams")
+    document_words_bigrams_embedding = make_dense_branch(document_words_bigrams_input, dense_num, hidden_size, dict_size, activation)
+
+    # shape: (BATCH_SIZE, dict_size)
+    query_symbol_trigrams_input = Input(shape=(dict_size,), sparse=True, name="query_symbol_trigrams")
+    query_symbol_trigrams_embedding = make_dense_branch(query_symbol_trigrams_input, dense_num, hidden_size, dict_size, activation)
+    # shape: (BATCH_SIZE, hidden_size)
+    document_symbol_trigrams_input = Input(shape=(dict_size,), sparse=True, name="document_symbol_trigrams")
+    document_symbol_trigrams_embedding = make_dense_branch(document_symbol_trigrams_input, dense_num, hidden_size, dict_size, activation)
+
+    query_embedding = concatenate([query_symbol_trigrams_embedding, query_words_bigrams_embedding, query_words_embedding], axis=1)
+    document_embedding = concatenate([document_symbol_trigrams_embedding, document_words_bigrams_embedding, document_words_embedding], axis=1)
+    
+    return make_compiled_model(
+        [
+            query_words_input,
+            document_words_input,
+            query_words_bigrams_input,
+            document_words_bigrams_input,
+            query_symbol_trigrams_input,
+            document_symbol_trigrams_input,
+        ],
+        query_embedding,
+        document_embedding
+    )
 
 def make_lstm_dense_model(document_dict_size,
                      document_dense_num,
@@ -267,7 +381,7 @@ def make_lstm_dense_model(document_dict_size,
         activation
     )
 
-    return make_compiled_model(query_input, document_input, query_embedding, document_embedding)
+    return make_compiled_model([query_input, document_input], query_embedding, document_embedding)
 
 
 class EvaluateCallback(Callback):
@@ -336,10 +450,10 @@ class EvaluateCallback(Callback):
         self._epoch += 1
 
 
-def train_epoch(model, batches_to_sample_negatives, epoch, input_from_lines_function):
+def train_epoch(model, batches_to_sample_negatives, epoch, positive_file, negative_file, input_from_lines_function):
     train_data_generator = make_generator(DataGenerator(
-        "freq_positive_train_data.tsv",
-        "freq_negative_train_data.tsv",
+        positive_file,
+        negative_file,
         model,
         BATCH_SIZE,
         BATCH_SIZE,
@@ -348,8 +462,8 @@ def train_epoch(model, batches_to_sample_negatives, epoch, input_from_lines_func
     ))
 
     val_data_generator = make_generator(DataGenerator(
-        "freq_positive_train_data.tsv",
-        "freq_negative_train_data.tsv",
+        positive_file,
+        negative_file,
         model,
         BATCH_SIZE,
         BATCH_SIZE,
@@ -358,8 +472,8 @@ def train_epoch(model, batches_to_sample_negatives, epoch, input_from_lines_func
     ))
 
     test_data_generator = make_generator(DataGenerator(
-        "freq_positive_train_data_10K.tsv",
-        "freq_negative_train_data_10K.tsv",
+        positive_file,
+        negative_file,
         model,
         BATCH_SIZE,
         BATCH_SIZE,
@@ -384,5 +498,5 @@ def train_epoch(model, batches_to_sample_negatives, epoch, input_from_lines_func
         epochs=1,
         verbose=1,
         initial_epoch=0,
-        callbacks=[сallback]
+        callbacks=[сallback][:0]
     )
