9cd7b7b91e8ff6faf12036295aea37c9b39f48f5
Pavel Koshkin <koshkin.pasha@gmail.com>
2019-04-07 18:47:55 +0300

    sth

--- task_7/main.cpp
+++ task_7/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <vector>
 #include <climits>
+#include <bitset>
 
 
 const std::size_t HANDLING_BITS = 3;
@@ -13,74 +14,47 @@ template<typename T>
 struct BitsHandler {
 public:
     T data;
-    std::size_t ending_zeros_number;
-    std::size_t full_bytes_number;
+    std::size_t significant_digits_number;
     std::size_t first_byte_bits_number;
-    std::size_t last_byte_bits_number;
     std::size_t bytes_number;
 
-    BitsHandler(T in_data) :
-            data(in_data),
-            ending_zeros_number(0),
-            full_bytes_number(0),
-            first_byte_bits_number(0),
-            last_byte_bits_number(0),
-            bytes_number(0)
-    {
-        while (in_data % 2 == 0) {
-            in_data >>= 1;
-            ++ending_zeros_number;
-        }
-
-        std::size_t significant_digits_number = 0;
-
+    BitsHandler(T in_data) : data(in_data), significant_digits_number(0), first_byte_bits_number(0), bytes_number(0) {
         while (in_data > 0) {
             in_data >>= 1;
             ++significant_digits_number;
         }
-
-        bytes_number = 1;
-        if (significant_digits_number <= BITS_IN_FIRST_BYTE) {
-            first_byte_bits_number = significant_digits_number;
+        if (significant_digits_number % CHAR_BIT > 0) {
+            first_byte_bits_number = significant_digits_number % CHAR_BIT;
+            bytes_number = significant_digits_number / CHAR_BIT + 1;
         } else {
-            first_byte_bits_number = BITS_IN_FIRST_BYTE;
-            significant_digits_number -= BITS_IN_FIRST_BYTE;
-            last_byte_bits_number = significant_digits_number % CHAR_BIT;
-            full_bytes_number = significant_digits_number / CHAR_BIT;
-            bytes_number += full_bytes_number;
-            if (last_byte_bits_number > 0) {
-                bytes_number += 1;
-            }
+            first_byte_bits_number = CHAR_BIT;
+            bytes_number = significant_digits_number / CHAR_BIT;
         }
-    }
 
-    char get_first_byte() {
-        unsigned char bytes_number_bits = (bytes_number - 1) << BITS_IN_FIRST_BYTE;
-        unsigned char first_byte = data >> (ending_zeros_number + last_byte_bits_number + CHAR_BIT * full_bytes_number);
+        if (first_byte_bits_number > BITS_IN_FIRST_BYTE) {
+            first_byte_bits_number %= BITS_IN_FIRST_BYTE;
+            ++bytes_number;
+        }
         std::cout << "bytes number: " << bytes_number << std::endl;
-        std::cout << "bytes number bits : " << (int)bytes_number_bits << std::endl;
-        std::cout << "first byte: " << (int)first_byte << std::endl;
-        std::cout << "sum: " << (int)(bytes_number_bits | first_byte) << std::endl;
-        return bytes_number_bits | first_byte;
+        std::cout << "first_byte_bits_number: " << first_byte_bits_number << std::endl;
+        std::cout << "significant_digits_number: " << significant_digits_number << std::endl;
     }
 
-    char get_full_byte(size_t index) {
-        assert(full_bytes_number > index);
-        return (data >> (ending_zeros_number + last_byte_bits_number + CHAR_BIT * (full_bytes_number - index))) & UCHAR_MAX;
-    }
-
-    char get_last_byte() {
-        assert(last_byte_bits_number > 0);
-        return (data >> ending_zeros_number) & (UCHAR_MAX >> (CHAR_BIT - last_byte_bits_number));
+    char get_byte(size_t index) {
+        if (index == 0) {
+            unsigned char bytes_number_bits = ((bytes_number - 1) << BITS_IN_FIRST_BYTE);
+            std::cout << "bytes_number_bits: " << (int)bytes_number_bits << std::endl;
+            return  bytes_number_bits | (data >> (significant_digits_number - first_byte_bits_number));
+        } else if (index == 1) {
+            return UCHAR_MAX & (data >> (significant_digits_number - first_byte_bits_number - (index - 1) * CHAR_BIT));
+        } else {
+            return UCHAR_MAX & (data >> (significant_digits_number - first_byte_bits_number - (index - 1) * CHAR_BIT));
+        }
     }
 
     void add_bytes(std::vector<char>& bytes) {
-        bytes.push_back(get_first_byte());
-        for (size_t index = 0; index < full_bytes_number; ++index) {
-            bytes.push_back(get_full_byte(index));
-        }
-        if (last_byte_bits_number > 0) {
-            bytes.push_back(get_last_byte());
+        for (size_t index = 0; index < bytes_number; ++index) {
+            bytes.push_back(get_byte(index));
         }
     }
 };
@@ -124,24 +98,11 @@ std::size_t read_object(const char* data, std::vector<T>& objects) {
     //std::cout << "bytes number in read_object: " << bytes_number << std::endl;
 
     object += data[0] & (UCHAR_MAX >> HANDLING_BITS);
-    for (size_t i = 1; i < bytes_number - 1; ++i) {
+    for (size_t i = 1; i < bytes_number; ++i) {
         std::cout << "object before: " << object << std::endl;
         object <<= CHAR_BIT;
         object += data[i];
         std::cout << "object after: " << object << std::endl;
-    }
-
-    std::size_t significant_digits_num = get_significant_digits_num(data[bytes_number - 1]);
-    std::cout << "significant_digits_num: " << significant_digits_num << std::endl;
-    std::cout << "object before: " << object << std::endl;
-    object <<= significant_digits_num;
-    object += data[bytes_number - 1];
-    std::cout << "object after: " << object << std::endl;
-
-    std::cout << "zero bytes: " << sizeof(T) - bytes_number - 1 << std::endl;
-
-    for (int i = 0; i < sizeof(T) - bytes_number - 1; ++i) {
-        object <<= CHAR_BIT;
     }
 
     objects.push_back(object);
@@ -159,7 +120,16 @@ std::vector<T> decode(const char* data, std::size_t length) {
 }
 
 int main () {
-    std::uint64_t number = 251658240;
+    std::uint64_t num = 511;
+    char* char_p = (char*)&num;
+    for (std::size_t i = 0; i < sizeof(std::uint64_t); ++i) {
+        std::cout << std::bitset<8>(char_p[i]) << std::endl;
+    }
+    std::cout << "----------------------" << std::endl;
+
+
+    std::uint64_t number;
+    std::cin >> number;
     std::cout << "number: " << number << std::endl;
     std::vector<char> bytes = encode(&number, 1);
     for (int i = 0; i < bytes.size(); ++i) {
